8c69d67c34bc018a9332bbe19aa9f98e19cd85cb
Thomas Henricus Leonard van de Ven <83241213+Thomasvandeven@users.noreply.github.com>
2022-03-02 20:05:26 +0100

    Delete SimpleWebShop/src/main directory

--- SimpleWebShop/src/main/java/com/example/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.module.webshop.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.module.webshop.entity.Category;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-  Category findByName(String name);
-}
